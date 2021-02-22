98269b387ddbbb2895ce1a3274835a2ae923d2b7
Driton Latifi <drit0046@zbc.dk>
2021-02-22 11:18:26 +0100

    program

---
+++ Geometri/Kvadrat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    class Kvadrat
+    {
+        private double side { get; set; }
+        public Kvadrat(double længde)
+        {
+            side = længde;
+        }
+
+        public Kvadrat()
+        {
+
+        }
+
+        public double Areal()
+        {
+            double areal = Math.Pow(side, 2);
+            return areal;
+        }
+
+        public double Omkreds()
+        {
+            return 4 * side;
+        }
+    }
+}
---
+++ Geometri/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kvadrat kvadrat1 = new Kvadrat(90);
+            Kvadrat kvadrat2 = new Kvadrat(20);
+
+            Console.WriteLine(kvadrat1.Omkreds());
+            Console.WriteLine(kvadrat2.Areal());
+            Console.ReadKey();
+        }
+    }
+}
